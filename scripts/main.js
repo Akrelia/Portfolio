6d83aec039e46037ee7613b3159608e8ca1f4b48
CHAUDRON Romain <romain.richard.chaudron@gmail.com>
2023-10-02 17:39:04 +0200

    Before modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,61 @@
+let scrollContainer = document.querySelector(".gallery");
+var cards = document.querySelectorAll(".card3d");
+
+function Card3D(card, ev) {
+  let img = card.querySelector("img");
+  let imgRect = card.getBoundingClientRect();
+  let width = imgRect.width;
+  let height = imgRect.height;
+  let mouseX = ev.offsetX;
+  let mouseY = ev.offsetY;
+  let rotateY = map(mouseX, 0, width, -25, 25);
+  let rotateX = map(mouseY, 0, height, 20, -30);
+  let brightness = map(mouseY, 0, height, 1.5, 0.5);
+  let blur = map(mouseY, 0, height, 0, 1);
+
+  img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  img.style.filter = `brightness(${brightness})`;
+  // img.style.filter = `blur(${blur}px)`;
+}
+
 function map(val, minA, maxA, minB, maxB) {
-    return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
-  }
-  
-  function Card3D(card, ev) {
-    let img = card.querySelector('img');
-    let imgRect = card.getBoundingClientRect();
-    let width = imgRect.width;
-    let height = imgRect.height;
-    let mouseX = ev.offsetX;
-    let mouseY = ev.offsetY;
-    let rotateY = map(mouseX, 0, width, -25, 25);
-    let rotateX = map(mouseY, 0, height, 20, -30);
-    let brightness = map(mouseY, 0, height, 1.5, 0.5);
-    let blur = map(mouseY,0,height,0,1);
-    
-    img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    img.style.filter = `brightness(${brightness})`;
-   // img.style.filter = `blur(${blur}px)`;
-  }
-  
-  var cards = document.querySelectorAll('.card3d');
-  
-  cards.forEach((card) => {
-    card.addEventListener('mousemove', (ev) => {
-      Card3D(card, ev);
-    });
-    
-    card.addEventListener('mouseleave', (ev) => {
-      let img = card.querySelector('img');
-      
-      img.style.transform = 'rotateX(0deg) rotateY(0deg)';
-      img.style.filter = 'brightness(1)';
-      img.style.filter = 'blur(0px)';
-    });
+  return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+}
+
+cards.forEach((card) => {
+  card.addEventListener("mousemove", (ev) => {
+    Card3D(card, ev);
   });
 
-  let scrollContainer = document.querySelector(".gallery");
-  let backBtn = document.getElementById("backBtn");
-  let nextBtn = document.getElementById("nextBtn");
+  card.addEventListener("mouseleave", (ev) => {
+    let img = card.querySelector("img");
 
-  scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-  })
+    img.style.transform = "rotateX(0deg) rotateY(0deg)";
+    img.style.filter = "brightness(1)";
+    img.style.filter = "blur(0px)";
+  });
+});
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+function showModal() {
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
